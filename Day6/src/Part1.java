5a3a33e5d739e0ccf9c11ab52ea700c511ed7b21
tushardahal9 <tushardahal9@gmail.com>
2023-12-29 19:53:30 +0000

    Day 6 Part 1 added

---
+++ Day6/src/Part1.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class Part1 {
+
+    public static ArrayList<ArrayList<Integer>> getTimesAndDistances() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("./src/input.txt"));
+
+            String[] timeString = reader.readLine().split(":")[1].trim().split(" {5}");
+            String[] distanceString = reader.readLine().split(":")[1].trim().split(" {3}");
+
+            return getValues(timeString, distanceString);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static ArrayList<ArrayList<Integer>> getValues(String[] timeString, String[] distanceString) {
+        ArrayList<Integer> times = new ArrayList<>();
+        ArrayList<Integer> distances = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> values = new ArrayList<>();
+
+        for (String time : timeString) {
+            times.add(Integer.parseInt(time.trim()));
+        }
+        for (String distance : distanceString) {
+            distances.add(Integer.parseInt(distance.trim()));
+        }
+
+        values.add(times);
+        values.add(distances);
+        return values;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<ArrayList<Integer>> values = getTimesAndDistances();
+        int answer = 1;
+
+        for (int i = 0; i < 4; i++) {
+            int count = 0;
+            int time = values.getFirst().get(i);
+            int distance = values.getLast().get(i);
+            for (int j = 0; j <= time; j++) {
+                int distanceTravelled = j * (time - j);
+                if (distanceTravelled > distance) count++;
+            }
+            answer *= count;
+        }
+        System.out.println(answer);
+    }
+}
