04061a797b3fd8a7ada8871713a2e802e7af9f26
tushardahal9 <tushardahal9@gmail.com>
2023-12-31 15:55:00 +0000

    Day 9 both parts added

---
+++ Day9/src/Part1/Part1.java
@@ -0,0 +1,23 @@
+package Part1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Part1 {
+    public static void main(String[] args) {
+        try {
+            File input = new File("./src/input.txt");
+            Scanner reader = new Scanner(input);
+            long answer = 0;
+
+            while (reader.hasNextLine()) {
+                String line = reader.nextLine();
+                answer += new Report(line).getHistoryValue();
+            }
+            System.out.println(answer);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ Day9/src/Part2/Report.java
@@ -0,0 +1,57 @@
+package Part2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Report {
+    private ArrayList<Long> report;
+
+    public Report(String report) {
+        this.report = new ArrayList<>();
+        String[] splitReport = report.split(" ");
+        for (String s : splitReport) {
+            this.report.add(Long.parseLong(s));
+        }
+    }
+
+    public ArrayList<Long> getReport() {
+        return report;
+    }
+
+    public ArrayList<ArrayList<Long>> getSteps() {
+        ArrayList<Long> currentReport = report;
+        ArrayList<ArrayList<Long>> allSteps = new ArrayList<>();
+        allSteps.add(this.getReport());
+
+        while (!checkIfZero(currentReport)) {
+
+            ArrayList<Long> currentSteps = new ArrayList<>();
+
+            for (int i = 0; i <= currentReport.size() - 2; i++) {
+                currentSteps.add(currentReport.get(i + 1) - currentReport.get(i));
+            }
+            allSteps.add(currentSteps);
+            currentReport = currentSteps;
+        }
+        return allSteps;
+    }
+
+    public boolean checkIfZero(ArrayList<Long> input) {
+        for (long num : input) {
+            if (num != 0) return false;
+        }
+        return true;
+    }
+
+    public long getHistoryValue() {
+        ArrayList<ArrayList<Long>> steps = getSteps();
+        Collections.reverse(steps);
+
+        long stepModifier = 0;
+        for (ArrayList<Long> currentSteps : steps) {
+            currentSteps.addFirst(currentSteps.getFirst() - stepModifier);
+            stepModifier = currentSteps.getFirst();
+        }
+        return steps.getLast().getFirst();
+    }
+}
